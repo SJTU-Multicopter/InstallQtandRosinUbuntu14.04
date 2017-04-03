37769a237447f4d2eeccb21606673ed2c3bc6457
g-ch <947089399@qq.com>
2017-04-03 13:29:56 +0800

    ros kinetic

--- ros-kinetic.sh
+++ ros-kinetic.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-#Author: Clarence Chan, Sept 23th, 2016
+#Author: Clarence Chan, Otc 22th, 2016
 #E-Mail: 947089399@qq.com
 
 #keys, environment
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get update
-echo "Keys settled. Install ROS now!"
+echo "Keys settled. Install ROS kinetic now!"
 
-#ros-jade
-sudo apt-get install ros-jade-desktop-full
-sed -i '$a source /opt/ros/jade/setup.bash' ~/.bashrc
-echo "Ros Jade installed successfully. Install some accessories."
+#ros-kinetic
+sudo apt-get install ros-kinetic-desktop-full
+sed -i '$a source /opt/ros/kinetic/setup.bash' ~/.bashrc
+echo "Ros kinetic installed successfully. Install some accessories."
 
 #archives
-source /opt/ros/jade/setup.bash
+source /opt/ros/kinetic/setup.bash
 sudo rosdep init
 rosdep update
 sudo apt-get install python-rosinstall
@@ -23,7 +23,7 @@ echo "Accessories installed successfully. Start building workspace."
 #workspace
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/jade/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ..
 catkin_make
